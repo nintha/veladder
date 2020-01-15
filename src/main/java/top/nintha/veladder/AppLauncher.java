4a0575f105281da5127fe06c6ee203ae0dceff68
ninthakeey <ninthakeey@hotmail.com>
2020-01-15 15:52:03 +0800

    Update AppLauncher.java

--- src/main/java/top/nintha/veladder/AppLauncher.java
+++ src/main/java/top/nintha/veladder/AppLauncher.java
@@ -293,6 +293,7 @@ public class AppLauncher extends AbstractVerticle {
     }
 
     public static void main(String[] args) {
+        System.setProperty("vertx.disableDnsResolver", "true");
         System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory");
         Vertx vertx = Vertx.vertx();
         vertx.deployVerticle(new AppLauncher());
