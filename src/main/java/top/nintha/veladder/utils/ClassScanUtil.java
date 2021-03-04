84bce2a197862bb6d120ca4386e2dc9221644489
ninthakeey <ninthakeey@hotmail.com>
2021-03-04 19:54:16 +0800

    feat: package scan unit test

--- src/main/java/top/nintha/veladder/utils/ClassScanUtil.java
+++ src/main/java/top/nintha/veladder/utils/ClassScanUtil.java
@@ -1,6 +1,6 @@
 package top.nintha.veladder.utils;
 
-import top.nintha.veladder.annotations.RestController;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,115 +13,118 @@ import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
+@Slf4j
 public class ClassScanUtil {
+    private static final String CLASS_FILE_SUFFIX = ".class";
 
     /**
      * 扫描指定包路径下所有包含指定注解的类
      *
      * @param packageName 包名
-     * @param apiClass    指定的注解
+     * @param annotation  指定的注解
      * @return Set
      */
-    public static <A extends Annotation> Set<Class<?>> scanByAnnotation(String packageName, Class<A> apiClass) {
-        Set<Class<?>> classSet = new HashSet<>();
-        // 获取包的名字 并进行替换
+    public static <A extends Annotation> Set<Class<?>> scanByAnnotation(String packageName, Class<A> annotation) {
+        final Set<Class<?>> classSet = new HashSet<>();
         String packageDirName = packageName.replace('.', '/');
-        // 定义一个枚举的集合 并进行循环来处理这个目录下的things
-        Enumeration<URL> dirs;
+        final Enumeration<URL> dirs;
         try {
             dirs = Thread.currentThread().getContextClassLoader().getResources(packageDirName);
-            // 循环迭代下去
-            while (dirs.hasMoreElements()) {
-                // 获取下一个元素
-                URL url = dirs.nextElement();
-                // 得到协议的名称
-                String protocol = url.getProtocol();
-                // 如果是以文件的形式保存在服务器上
-                if ("file".equals(protocol)) {
-                    // 获取包的物理路径
-                    String filePath = URLDecoder.decode(url.getFile(), StandardCharsets.UTF_8);
-                    // 以文件的方式扫描整个包下的文件 并添加到集合中
-                    File dir = new File(filePath);
-                    List<File> fileList = new ArrayList<File>();
-                    fetchFileList(dir, fileList);
-                    for (File f : fileList) {
-                        String fileName = f.getAbsolutePath();
-                        if (fileName.endsWith(".class")) {
-                            String noSuffixFileName = fileName.substring(8 + fileName.lastIndexOf("classes"), fileName.indexOf(".class"));
-                            String filePackage = noSuffixFileName.replaceAll("\\\\", ".");
-                            Class<?> clazz = Class.forName(filePackage);
-                            if (null != clazz.getAnnotation(apiClass)) {
-                                classSet.add(clazz);
-                            }
-                        }
-                    }
-                } else if ("jar".equals(protocol)) {
-                    // 如果是jar包文件
-                    // 定义一个JarFile
-                    JarFile jar;
-                    try {
-                        // 获取jar
-                        System.out.println(url);
-                        //jar:file:/D:/MyStudy/apidoc/target/apidoc-0.0.1-SNAPSHOT.jar!/BOOT-INF/classes!/com/demo/web
-                        JarURLConnection urlConnection = (JarURLConnection) url.openConnection();
-                        jar = urlConnection.getJarFile();
-                        // 从此jar包 得到一个枚举类
-                        Enumeration<JarEntry> entries = jar.entries();
-                        // 同样的进行循环迭代
-                        while (entries.hasMoreElements()) {
-                            // 获取jar里的一个实体 可以是目录 和一些jar包里的其他文件 如META-INF等文件
-                            JarEntry entry = entries.nextElement();
-                            String name = entry.getName();
-                            // 如果是以/开头的
-                            if (name.charAt(0) == '/') {
-                                // 获取后面的字符串
-                                name = name.substring(1);
-                            }
-                            // 如果前半部分和定义的包名相同
-                            if (name.startsWith(packageDirName)) {
-                                int idx = name.lastIndexOf('/');
-                                // 如果以"/"结尾 是一个包
-                                if (idx != -1) {
-                                    // 获取包名 把"/"替换成"."
-                                    packageName = name.substring(0, idx).replace('/', '.');
-                                }
-                                // 如果可以迭代下去 并且是一个包
-                                // 如果是一个.class文件 而且不是目录
-                                if (name.endsWith(".class") && !entry.isDirectory()) {
-                                    // 去掉后面的".class" 获取真正的类名
-                                    String className = name.substring(packageName.length() + 1, name.length() - 6);
-                                    try {
-                                        Class<?> cls = Class.forName(packageName + '.' + className);
-                                        if (null != cls.getAnnotation(apiClass)) {
-                                            classSet.add(cls);
-                                        }
-                                    } catch (ClassNotFoundException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+        } catch (IOException e) {
+            log.warn("[ClassScan] failed to get package resources, packageDirName={}", packageDirName, e);
+            return classSet;
+        }
+        while (dirs.hasMoreElements()) {
+            // 获取下一个元素
+            URL url = dirs.nextElement();
+            // 得到协议的名称
+            String protocol = url.getProtocol();
+            Set<String> classNames = Collections.emptySet();
+            // 如果是以文件的形式保存在服务器上
+            if ("file".equals(protocol)) {
+                classNames = scanFile(url, packageName);
+            }
+            // 如果是jar包文件
+            else if ("jar".equals(protocol)) {
+                classNames = scanJar(url, packageName, packageDirName);
+            }
+            for (String cls : classNames) {
+                Class<?> clazz = null;
+                try {
+                    clazz = Class.forName(cls);
+                } catch (ClassNotFoundException e) {
+                    log.warn("[ClassScan] failed to load class '{}'", cls, e);
+                }
+                if (clazz != null && null != clazz.getAnnotation(annotation)) {
+                    classSet.add(clazz);
                 }
             }
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
+
         }
         return classSet;
+    }
+
+    private static Set<String> scanFile(URL url, String packageName) {
+        // 获取包的物理路径
+        String filePath = URLDecoder.decode(url.getFile(), StandardCharsets.UTF_8);
+        // 以文件的方式扫描整个包下的文件 并添加到集合中
+        File dir = new File(filePath);
+        Set<String> packages = new HashSet<>();
+        fetchFilePackages(dir, packageName, packages);
+        return packages;
+    }
+
+    private static Set<String> scanJar(URL url, String packageName, String packageDirName) {
+        JarFile jar;
+        try {
+            JarURLConnection urlConnection = (JarURLConnection) url.openConnection();
+            jar = urlConnection.getJarFile();
+        } catch (IOException e) {
+            log.warn("[ClassScan] failed to resolve jar entry", e);
+            return Set.of();
+        }
+
+        Set<String> classNames = new HashSet<>();
+        Enumeration<JarEntry> entries = jar.entries();
+        while (entries.hasMoreElements()) {
+            // 获取jar里的一个实体 可以是目录 和一些jar包里的其他文件 如META-INF等文件
+            JarEntry entry = entries.nextElement();
+            String name = entry.getName();
+            // 如果是以/开头的
+            if (name.charAt(0) == '/') {
+                // 获取后面的字符串
+                name = name.substring(1);
+            }
+            // 如果前半部分和定义的包名相同
+            if (name.startsWith(packageDirName)) {
+                int idx = name.lastIndexOf('/');
+                // 如果以"/"结尾 是一个包
+                if (idx != -1) {
+                    // 获取包名 把"/"替换成"."
+                    packageName = name.substring(0, idx).replace('/', '.');
+                }
+                // 如果可以迭代下去 并且是一个包
+                // 如果是一个.class文件 而且不是目录
+                if (name.endsWith(".class") && !entry.isDirectory()) {
+                    // 去掉后面的".class" 获取真正的类名
+                    String className = name.substring(packageName.length() + 1, name.length() - 6);
+                    classNames.add(packageName + '.' + className);
+                }
+            }
+        }
+        return classNames;
     }
 
     /**
      * 查找所有的文件
      */
-    private static void fetchFileList(File dir, List<File> fileList) {
+    private static void fetchFilePackages(File dir, String packageName, Set<String> packages) {
         if (dir.isDirectory()) {
             for (File f : Objects.requireNonNull(dir.listFiles())) {
-                fetchFileList(f, fileList);
+                fetchFilePackages(f, String.format("%s.%s", packageName, f.getName()), packages);
             }
-        } else {
-            fileList.add(dir);
+        } else if (packageName.endsWith(CLASS_FILE_SUFFIX)) {
+            packages.add(packageName.substring(0, packageName.length() - CLASS_FILE_SUFFIX.length()));
         }
     }
 
